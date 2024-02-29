d7354f3d9e4794441918f9d5e80045a49857c993
mikhailshauchenka <mike1shef@gmail.com>
2024-02-29 15:42:13 +0100

    Initial commit

---
+++ Photo Editor/task/src/main/java/org/hyperskill/photoeditor/MainActivity.kt
@@ -0,0 +1,63 @@
+package org.hyperskill.photoeditor
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var currentImage: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        bindViews()
+
+        //do not change this line
+        currentImage.setImageBitmap(createBitmap())
+    }
+
+    private fun bindViews() {
+        currentImage = findViewById(R.id.ivPhoto)
+    }
+
+    // do not change this function
+    fun createBitmap(): Bitmap {
+        val width = 200
+        val height = 100
+        val pixels = IntArray(width * height)
+        // get pixel array from source
+
+        var R: Int
+        var G: Int
+        var B: Int
+        var index: Int
+
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                // get current index in 2D-matrix
+                index = y * width + x
+                // get color
+                R = x % 100 + 40
+                G = y % 100 + 80
+                B = (x+y) % 100 + 120
+
+                pixels[index] = Color.rgb(R,G,B)
+
+            }
+        }
+        // output bitmap
+        val bitmapOut = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
+        bitmapOut.setPixels(pixels, 0, width, 0, 0, width, height)
+        return bitmapOut
+    }
+}
